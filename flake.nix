c84fb7df990c9e4e949b9ffe63d5e288efe2172a
Mark Dietzer <git@doridian.net>
2025-11-17 21:52:59 -0800

    Reduce import sillyness (#30)

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     nix-jekyll-builder.url = "git+https://git.chobble.com/chobble/nix-jekyll-builder";
+    nix-jekyll-builder.inputs.nixpkgs.follows = "nixpkgs";
+    nix-jekyll-builder.inputs.flake-utils.follows = "flake-utils";
   };
 
   # Regenerate gemset.nix: nix-shell -p bundix --run "bundix -l"
-  outputs = { self, flake-utils, nix-jekyll-builder }:
+  outputs = { flake-utils, nix-jekyll-builder, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         package =  (nix-jekyll-builder.lib.mkJekyllSite{
